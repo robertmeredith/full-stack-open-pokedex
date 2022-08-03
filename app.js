f679c5ce7119401991cd30932545a81c3cc06467
robertmeredith <sushiaudio@hotmail.com>
2022-08-03 16:53:07 +1000

    added healthcheck to pipeline

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT || 5001
 // use the 'dist' folder for all routes
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('HEALTHY APP')
+})
+
+app.get('/version', (req, res) => {
+  res.send('VERSION 1')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
