b18dfdd619050f32b148d6f0f4d4d13231b7cb62
robertmeredith <sushiaudio@hotmail.com>
2022-08-03 16:04:34 +1000

    added second e2e test to find pikachu

--- cypress/e2e/pokedex-app.cy.js
+++ cypress/e2e/pokedex-app.cy.js
@@ -1,9 +1,16 @@
 describe('Pokedex', function () {
-  it('front page can be opened', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5001')
+  })
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+
+  it('individual Pokemon page can be opened', function () {
+    cy.contains('pikachu').click();
+    cy.contains('lightning rod')
   })
 })
