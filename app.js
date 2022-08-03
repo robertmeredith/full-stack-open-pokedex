d7c9758508109076f27698d432b2385b2581ff36
robertmeredith <sushiaudio@hotmail.com>
2022-08-03 14:35:41 +1000

    fixed linting errors

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001
 
 // use the 'dist' folder for all routes
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
